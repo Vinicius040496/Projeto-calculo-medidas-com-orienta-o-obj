6674b4101785ee0ba76110b9c7a8a30076b234f2
Vinicius040496 <vinicius.almeida2011@gmail.com>
2024-06-03 19:16:23 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp125/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp125
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo Medidas = new Retangulo();
+
+
+          
+            Console.WriteLine("Entre com a largura e altura do retangulo: ");
+            Medidas.Altura = double.Parse(Console.ReadLine());
+            Medidas.Largura = double.Parse(Console.ReadLine());
+            Medidas.Area();
+            Medidas.Perimetro();
+            Medidas.Diagonal();
+            Medidas.Perimetro();
+            Console.WriteLine(Medidas);
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ ConsoleApp125/Retangulo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp125
+{
+    internal class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+        double Soma;
+
+        public double Area()
+        {
+            return Altura * Largura;
+        }
+
+        public double Perimetro()
+        {
+            return Altura + Altura + Largura + Largura;
+        }
+
+        public double Diagonal()
+        {
+         Soma = Area() * 2 + Largura * 2;
+            return Math.Sqrt(Soma);
+        }
+        public override string ToString()
+        {
+            return 
+                   "Area = "
+                   + Area().ToString("f2") + " Perimetro = "
+                   + Perimetro().ToString("f2") + " Diagonal = "
+                   + Diagonal().ToString("f2");
+
+        }
+    }
+    
+    
+}
